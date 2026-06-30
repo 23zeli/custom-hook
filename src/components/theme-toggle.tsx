d844ab0a30221149080231ae41149b68574f04b7
Mtutuzeli <mtutuzeli@gmail.com>
2026-06-30 11:39:42 +0200

    added uppercase logic

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import ResetButton from "./reset-button";
 
 export default function ThemeToggle() {
+    const [theme, setTheme] = useState('light')
+
     return (
         <section>
+            <p>Current theme:{theme.charAt(0).toUpperCase() + theme.slice(1)}</p>
             <button>Light Mode</button>
             <button>Dark Mode</button>
 
