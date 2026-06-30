5a32674611573f36b1e3238a2354f3bd08f0680e
Mtutuzeli <mtutuzeli@gmail.com>
2026-06-30 11:15:30 +0200

    created reset button component to prevent duplication

---
+++ src/components/reset-button.tsx
@@ -0,0 +1,9 @@
+export default function ResetButton () {
+    return (
+        <section>
+            <button className="bg-black text-white py-3 px-5">
+                Reset
+            </button>
+        </section>
+    )
+}
